41f72d77e400bbc71c7de6a8a11c2324474a8102
limiao <3209319055@qq.com>
2026-03-09 23:04:05 +0800

    Day3: Basic Loop Programs

--- src/Day3/GuessNumberGame.java
+++ src/Day3/GuessNumberGame.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 
 public class GuessNumberGame {
     public static void main(String[] args) {
-        Random random=new Random();
-        int b=random.nextInt(100)+1;
-        Scanner src=new Scanner(System.in);
+        Random random = new Random();
+        int b = random.nextInt(100) + 1;
+        Scanner src = new Scanner(System.in);
         System.out.print("从1-100猜一个数字:");
-        int num=src.nextInt();
-        int count=0;
-        for(int i=1;i<=num;i++)
-            count++;
-        System.out.printf("猜到啦,是%d!",count);
-
+        while(true) {
+            int num = src.nextInt();
+            if (num != b)
+                {if(num<b)
+                    System.out.print("猜小啦!再猜一次：");
+                 else
+                    System.out.print("猜大啦!再猜一次：");}
+            else
+                break;
+        }
+        System.out.print("恭喜你猜对啦！");
     }
 }
